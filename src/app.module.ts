1b5fd00d716493481db196a86bf42301a3576138
lifthus <lifthus531@gmail.com>
2024-01-17 12:08:18 +0900

    dev/nest hellow world api removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from 'src/api/api.module';
 
 @Module({
   imports: [ApiModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
