8b8ce836fe8c3de84a605bf1c0631e92f0e70bd0
lifthus <lifthus531@gmail.com>
2024-01-20 16:06:59 +0900

    #11/imp/new exception-filter applied to the app

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ServiceHTTPExceptionFilter } from 'src/common/exception/service/ServiceHTTPException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalFilters(new ServiceHTTPExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('The Second API')
